28570d83d20f7646d050845fe603141dfb0637e0
xiaodong <875697126@qq.com>
2016-10-21 17:12:32 +0800

    update

--- Typescript/basciTypes.ts
+++ Typescript/basciTypes.ts
@@ -8,3 +8,28 @@ let binary:number = 0b1010;
 let octal:number = 0o744;
 
 // String
+let color:string = 'blue';
+color = 'red';
+
+//template strings
+let fullName:string = 'Bob Bobbington';
+let age:number = 28;
+let sentence:string = `Hello , my name is ${ fullName }. I'll be ${age + 1} years old next year.`;
+
+// Array
+//type[]
+let list:number[] = [1,2,3];
+
+//Array<type>
+let list1:Array<number> = [1,2,3];
+
+// Tuple
+let x:[string, number];
+//initial 
+x = ["Hello", 10]; //类型必须匹配
+
+
+
+
+
+
