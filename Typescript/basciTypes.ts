59b5c475e2e4a35ea9558e9bfa613fd8c90aff71
xiaodong <875697126@qq.com>
2016-10-21 17:50:19 +0800

    base types over

--- Typescript/basciTypes.ts
+++ Typescript/basciTypes.ts
@@ -28,6 +28,26 @@ let x:[string, number];
 //initial 
 x = ["Hello", 10]; //类型必须匹配
 
+// Enum
+enum Color {Red=1, Green = 2, Blue = 4}; //2,4 array index
+let c:Color = Color.Green;
+let colorName:string = Color[2]; //Green
+
+// Any
+
+let notSure:any = 4;
+notSure.ifItExists()
+notSure.toFixed();
+
+let list2:any[] = [1,'tree',true];
+
+// Void
+
+function warnUser():void{
+    console.log('this is my warning');
+}
+
+
 
 
 
