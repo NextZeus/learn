3f089463da295fd165273520c6204bbb4285e347
xiaodong <875697126@qq.com>
2016-10-22 14:45:28 +0800

    class

---
+++ Typescript/class.ts
@@ -0,0 +1,150 @@
+class Greeter{
+    greeting:string;
+    constructor(message:string){
+        this.greeting = message;
+    }
+    greet(){
+        return 'Hello, '+this.greeting;
+    }
+}
+
+let greeting = new Greeter('world');
+
+// Inheritance
+class Animal{
+    private name:string;
+    public constructor(theName:string){
+        this.name = theName;
+    }
+    public move(distanceInMeters:number=0){
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+class Snake extends Animal{
+    constructor(name:string){
+        super(name);
+    }
+    move(distanceInMeters=5){
+        console.log('Slithering...');
+        super.move(distanceInMeters);
+    }
+}
+
+class Horse extends Animal {
+    constructor(name:string){
+        super(name);
+    }
+    move(distanceInMeters=45){
+        console.log("Galloping...");
+        super.move(distanceInMeters);
+    }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom:Animal = new Horse("Tommy the Palomino");
+sam.move();
+tom.move();
+
+// Accessors
+
+let passcode = "secret passcode";
+
+class Employee {
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+
+    set fullName(newName: string) {
+        if (passcode && passcode == "secret passcode") {
+            this._fullName = newName;
+        }
+        else {
+            console.log("Error: Unauthorized update of employee!");
+        }
+    }
+}
+
+let employee = new Employee();
+employee.fullName = "Bob Smith";
+if (employee.fullName) {
+    console.log(employee.fullName);
+}
+
+// Static Properties
+class Grid {
+    static origin = {x: 0, y: 0};//不需要创建实例对象 即可以使用
+    calculateDistanceFromOrigin(point: {x: number; y: number;}) {
+        let xDist = (point.x - Grid.origin.x);
+        let yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    }
+    constructor (public scale: number) { }
+}
+
+let grid1 = new Grid(1.0);  // 1x scale
+let grid2 = new Grid(5.0);  // 5x scale
+
+console.log(grid1.calculateDistanceFromOrigin({x: 10, y: 10}));
+console.log(grid2.calculateDistanceFromOrigin({x: 10, y: 10}));
+
+// Abstract Classes 抽象类
+abstract class Department {
+
+    constructor(public name: string) {
+    }
+
+    printName(): void {
+        console.log("Department name: " + this.name);
+    }
+
+    abstract printMeeting(): void; // must be implemented in derived classes 子类必须要实现
+    //抽象类中没有定义的方法， 子类中不能实现
+}
+
+class AccountingDepartment extends Department {
+
+    constructor() {
+        super("Accounting and Auditing"); // constructors in derived classes must call super()
+    }
+
+    printMeeting(): void {
+        console.log("The Accounting Department meets each Monday at 10am.");
+    }
+
+    generateReports(): void {
+        console.log("Generating accounting reports...");
+    }
+}
+
+let department: Department; // ok to create a reference to an abstract type
+// department = new Department(); // error: cannot create an instance of an abstract class
+department = new AccountingDepartment(); // ok to create and assign a non-abstract subclass
+department.printName();
+department.printMeeting();
+// department.generateReports(); // error: method doesn't exist on declared abstract type
+
+
+//技巧
+class Greeter1{
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+//可这样写
+let Greeter2 = (function(){
+    function Greeter(message){
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function(){
+        return 'Hello, '+ this.greeting;
+    }
+    return Greeter;
+})();
