3c7aac468e64f14217dfe038f8311ae8112e0141
xiaodong <875697126@qq.com>
2016-11-09 16:19:55 +0800

    improved download method

--- Python/request/img_response.py
+++ Python/request/img_response.py
@@ -14,6 +14,17 @@ def download_image():
     with open('demo.jpg','wb') as fd:
         for chunk in response.iter_content(128):
             fd.write(chunk)
+def download_image_improved():
+    #如果没有权限 伪造header {'User-Agent':}
+    url = "http://image.lxway.com/upload/0/b2/0b2b7eb5ce674e66c6a728e85afae0f3_thumb.jpg"
+    from contextlib import closing
+    #关闭流
+    with closing(requests.get(url,stream=True)) as response:
+        with open('demo1.jpg','wb') as fd:
+            for chunk in response.iter_content(128):
+                fd.write(chunk)
+
+
 
 if __name__ == '__main__':
-    download_image()
+    download_image_improved()
