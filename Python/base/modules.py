5473c0ace6501c8082eab4600d20636f40b4961a
xiaodong <875697126@qq.com>
2017-01-17 12:05:44 +0800

    add huaban

--- Python/base/modules.py
+++ Python/base/modules.py
@@ -1,5 +1,4 @@
 from dict import add
-
 add()
 
 import random
@@ -10,6 +9,9 @@ def download_web_iamge(url):
     full_name = str(name) + ".jpg"
     urllib.request.urlretrieve(url, full_name)
 
-download_web_iamge("http://img.hb.aicdn.com/86591874e81a53efa1dc66fb9c947648ed0fcca94fd78-HkqIrt_fw658")
+# download_web_iamge("http://img.hb.aicdn.com/86591874e81a53efa1dc66fb9c947648ed0fcca94fd78-HkqIrt_fw658")
 
 # http://xiaodongli0.wixsite.com/helloworld
+
+url = "http://hbimg.b0.upaiyun.com/4fb0003394d6499aa1885cc5551ef956c44cef09217e7-K4TNkg"
+print(url[len(url)-6:])
---
+++ Python/spider/huaban.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
+import urllib.request
+
+def download_web_iamge(url, file_name):
+    full_name = str(file_name) + ".jpg"
+    urllib.request.urlretrieve(url, full_name)
+
+def replace_script_variable(script, userid):
+    script = str(script)[8:len(str(script)) - 9]
+    script = script.replace('var', '')\
+        .replace(';', '')\
+        .replace('app.route()', '')\
+        .replace('view = app.view = $("page").hide()', '')\
+        .replace('app._csr = true', '')\
+        .replace('\n', '')\
+        .replace('app = app || {}', 'app={"page":{}}')\
+        .replace('app.page = app.page || {}', 'app.page = {}')\
+        .replace('app={"page":{}}app.page = {}app.page["$url"] = "/boards/' + str(userid) + '/"app.page["board"] = ', '')
+
+    return script
+
+def download_huaban_image_by_userId(userid):
+    base_image_url = 'http://hbimg.b0.upaiyun.com/'
+
+    url = "http://huaban.com/boards/"+str(userid)+"/"
+    html = requests.get(url)
+    plain_text = html.text
+    soup = BeautifulSoup(plain_text, 'html.parser')
+
+    for script in soup.findAll('script'):
+        regex = re.compile(r'\w*app.page\["board"]\w*')
+        if regex.findall(str(script)):
+            app = replace_script_variable(script, userid)
+            result = u'' + str(app)
+            # print('result: ', result)
+            obj = json.loads(result)
+            # print('obj: ', obj)
+            pins = obj['pins']
+            print('pins.length: ', len(pins))
+
+            for pin in pins:
+                print('pin:', pin['pin_id'], 'http://hbimg.b0.upaiyun.com/'+pin['file']['key'])
+                image_name = pin['file']['key']
+                image_url = base_image_url + image_name
+                file_name = image_name[len(image_name) - 6:]
+                print('file_name', file_name)
+                # download_web_iamge(image_url, file_name)
+            # pin_id file:{id, key, width,height}
+            # image_url: http://hbimg.b0.upaiyun.com/key
+
+download_huaban_image_by_userId(19232159)
+# http://huaban.com/boards/19232159/?max=877153997&limit=20&wfl=1
+# http://huaban.com/boards/19232159/?max=668394537&limit=20&wfl=1
