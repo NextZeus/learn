26ce8f91eebb56ab12a4c7f4bad5731d940c065a
NextZeus <875697126@qq.com>
2017-01-21 05:02:48 +0800

    分类页面分析完毕 获取到列表页主播数据

--- Python/spiderHuajiao/huajiao.py
+++ Python/spiderHuajiao/huajiao.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from bs4 import NavigableString
 import requests
 import re
 
@@ -35,6 +36,39 @@ def get_category_list(catgory_id):
     last_page = int(last_page_tag.get('tabindex')) - 1
     print('last_page: ', last_page)
 
-    return
+    main_list = soup.find_all('a', href=re.compile('/l/'))
+    # print('mail_list: ', main_list)
+
+    data = list()
+
+    for link in main_list:
+        person = dict()
+        userId = link.get('href')[3:]
+        person['userId'] = userId
+        children = link.children
+
+        for child in children:
+            if not isinstance(child, NavigableString):
+                attr = child.attrs['class'][0]
+                if attr == 'pic':
+                    # child是最外层div 就是child
+                    avatar = child.img.get('src')
+                    person['avatar'] = avatar
+                    # print('avatar: ', avatar)
+                elif attr == 'text':
+                    # 第一个div 主播昵称
+                    nickName = child.div.get_text()
+                    person['nickName'] = nickName
+                    # 获取span里的正在观看人数
+                    watches = child.find_all('span', 'num')[0].get_text()
+                    person['watches'] = watches
+                    # print('nickName: ', nickName)
+                    # print('watches: ', watches)
+                else:
+                    print('continue!')
+
+        print('person ', person)
+
+    return data
 
 get_category_list(999)
