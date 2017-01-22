8b86aff75438b061aa7af6ae3d37e56d7abf1cfd
xiaodong <875697126@qq.com>
2017-01-22 15:13:05 +0800

    update

--- Python/spiderHuajiao/huajiao.py
+++ Python/spiderHuajiao/huajiao.py
@@ -31,70 +31,6 @@ def get_category_url_by_id_and_pageno(catgory_id, pageno):
     catgory_url = "http://www.huajiao.com/category/" + str(catgory_id) + "pageno=" + str(pageno)
     return catgory_url
 
-# 获取直播分类列表数据
-def get_category_list(catgory_id):
-    catgory_url = "http://www.huajiao.com/category/" + str(catgory_id)
-    soup = get_soup_by_url(catgory_url)
-    # 获取最大分页数
-    page_tag = soup.find_all('li', "paginate_button last")
-    if len(page_tag) <= 0:
-        return []
-
-    last_page_tag = soup.find_all('li', "paginate_button last")[0]
-    last_page = int(last_page_tag.get('tabindex'))
-
-    data = list()
-
-    for pageno in range(1, last_page):
-        catgory_url = get_category_url_by_id_and_pageno(catgory_id, pageno)
-        soup = get_soup_by_url(catgory_url)
-        main_list = soup.find_all('a', href=re.compile('/l/'))
-
-        for link in main_list:
-            person = dict()
-            liveId = link.get('href')[3:]
-            userid = get_anchorid_by_liveid(liveId)
-            person['userid'] = userid
-            children = link.children
-
-            # todo 替换 get_anchor_info_by_userid
-            # person = get_anchor_info_by_userid(userid)
-
-            for child in children:
-                # child是最外层的两个div
-                if not isinstance(child, NavigableString):
-                    attr = child.attrs['class'][0]
-                    if attr == 'pic':
-                        avatar = child.img.get('src')
-                        person['avatar'] = avatar
-                        # print('avatar: ', avatar)
-                    elif attr == 'text':
-                        # 第一个div 主播昵称
-                        nickName = child.div.get_text()
-                        person['nickName'] = nickName
-                        # 获取span里的正在观看人数
-                        span_tag = child.find_all('span', 'num')
-                        if len(span_tag) <= 0:
-                            watches = 0
-                        else:
-                            watches = child.find_all('span', 'num')[0].get_text(strip=True)
-                        person['watches'] = watches
-                        # print('nickName: ', nickName)
-                        # print('watches: ', watches)
-                    else:
-                        print('continue!')
-            data.append(person)
-            # print('person ', person)
-
-    # print('length: ', len(data))
-    return data
-
-# categories = get_huajiao_video_categories()
-# print('花椒主播分类: ', categories)
-# for category in categories.keys():
-#     data = get_category_list(category)
-#     print('category ' + str(category)+' anchor-number: ', len(data))
-
 # 根据直播ID 获取主播用户ID
 def get_anchorid_by_liveid(liveid):
     url = "http://www.huajiao.com/l/" + str(liveid)
@@ -111,18 +47,20 @@ def get_anchor_info_by_userid(userid):
     url = "http://www.huajiao.com/user/" + str(userid)
     soup = get_soup_by_url(url)
 
-    #userinfo div
+    # userinfo div
     userInfo = soup.find('div', {'id': 'userInfo'})
 
     username = userInfo.find('h3').get_text(strip=True)
     person['username'] = username
-    #头像
+    # 头像
     avatar = userInfo.find('img').get('src')
     person['avatar'] = avatar
 
+    # 简介
     about = userInfo.find('p', 'about')
     person['about'] = about.get_text(strip=True)
 
+    # 等级
     level = userInfo.find('span', 'level')
     person['level'] = level.get_text(strip=True)
 
@@ -144,5 +82,36 @@ def get_anchor_info_by_userid(userid):
     print('person: ', person)
     return person
 
-userid = get_anchorid_by_liveid(71956509)
-get_anchor_info_by_userid(userid)
+# 获取直播分类列表数据
+def get_category_list(catgory_id):
+    catgory_url = "http://www.huajiao.com/category/" + str(catgory_id)
+    soup = get_soup_by_url(catgory_url)
+    # 获取最大分页数
+    page_tag = soup.find_all('li', "paginate_button last")
+    if len(page_tag) <= 0:
+        return []
+
+    last_page_tag = soup.find_all('li', "paginate_button last")[0]
+    last_page = int(last_page_tag.get('tabindex'))
+
+    data = list()
+
+    for pageno in range(1, last_page):
+        catgory_url = get_category_url_by_id_and_pageno(catgory_id, pageno)
+        soup = get_soup_by_url(catgory_url)
+        main_list = soup.find_all('a', href=re.compile('/l/'))
+
+        for link in main_list:
+            liveId = link.get('href')[3:]
+            userid = get_anchorid_by_liveid(liveId)
+            person = get_anchor_info_by_userid(userid)
+            data.append(person)
+
+    # print('length: ', len(data))
+    return data
+
+# categories = get_huajiao_video_categories()
+# print('花椒主播分类: ', categories)
+# for category in categories.keys():
+#     data = get_category_list(category)
+#     print('category ' + str(category)+' anchor-number: ', len(data))
